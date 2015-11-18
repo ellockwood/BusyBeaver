148ce10241f95ca71f61925ff9be99960bcf1efe
Connor <n7.collins@gmail.com>
2015-11-17 22:57:55 -0500

    Modified Recursion

--- src/BusyBeaver.java
+++ src/BusyBeaver.java
@@ -2,8 +2,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
